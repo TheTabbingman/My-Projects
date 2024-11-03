b6391aa009c66c8b5ce64dc03bffb054c4e8539d
TheTabbingMan <gamerboyjjh@outlook.com>
2024-11-03 16:51:32 -0600

    Add non binary check

--- Bin2Dec/script.js
+++ Bin2Dec/script.js
@@ -3,6 +3,10 @@ function bin2Dec(binary) {
   let previousBit = 0;
   for (let i = 0; i < binaryString.length; i++) {
     let currentBit = binaryString.charAt(i);
+    if (currentBit != 0 && currentBit != 1) {
+      document.getElementById("number").textContent = "Not a binary number";
+      return;
+    }
     previousBit = previousBit * 2 + parseInt(currentBit);
   }
   document.getElementById("number").textContent = previousBit;
