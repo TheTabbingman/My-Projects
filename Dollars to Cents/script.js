81a3278127d9ba1a0895edc394c7b85ef48ca617
TheTabbingMan <gamerboyjjh@outlook.com>
2024-11-05 16:47:25 -0600

    Initial Dollars to Cents

---
+++ Dollars to Cents/script.js
@@ -0,0 +1,16 @@
+function updateNumbers(id, text) {
+    document.getElementById(id).textContent = text;
+}
+
+function dollars2Cents(dollars) {
+    const cents = Number(dollars.toString().replace('.', ''))
+    updateNumbers("cents", "Cents: " + cents + "¢")
+    quarters = Math.floor(cents / 25);
+    updateNumbers("quarters", "Quarters: " + quarters);
+    dimes = Math.floor((cents % 25) / 10);
+    updateNumbers("dimes", "Dimes: " + dimes);
+    nickels = Math.floor(((cents % 25) % 10) / 5);
+    updateNumbers("nickels", "Nickels: " + nickels);
+    pennies = Math.floor((((cents % 25) % 10) % 5) / 1);
+    updateNumbers("pennies", "Pennies: " + pennies);
+}
