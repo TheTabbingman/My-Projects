921d50122a7cafcf2c14f45133eaaee0cfd7c60f
TheTabbingMan <gamerboyjjh@outlook.com>
2024-11-05 22:18:27 -0600

    Check if input isn't a number. Round to 2 decimal places. Simplify pennies math.

--- Dollars to Cents/script.js
+++ Dollars to Cents/script.js
@@ -3,14 +3,24 @@ function updateNumbers(id, text) {
 }
 
 function dollars2Cents(dollars) {
-  const cents = Number(dollars.toString().replace(/[$.]/g, ""));
+  // Remove $ from string and convert to number
+  let cents = Number(dollars.toString().replace(/\$/g, ""));
+  // round to 2 decimal points and remove decimal point
+  cents = Math.round((cents + Number.EPSILON) * 100);
+  // Check cents is a number
+  if (isNaN(cents)) {
+    updateNumbers("cents", "Not a number");
+    return;
+  }
+  // Check amount of coins for each value
   const quarters = Math.floor(cents / 25);
   const dimes = Math.floor((cents % 25) / 10);
-  const nickels = Math.floor((cents % 25 % 10) / 5);
-  const pennies = Math.floor(cents % 25 % 10 % 5);
+  const nickels = Math.floor(((cents % 25) % 10) / 5);
+  const pennies = cents % 5;
+  // update display for each coin value
   updateNumbers("cents", "Cents: " + cents + "¢");
-  updateNumbers("pennies", "Pennies: " + pennies);
   updateNumbers("quarters", "Quarters: " + quarters);
-  updateNumbers("nickels", "Nickels: " + nickels);
   updateNumbers("dimes", "Dimes: " + dimes);
+  updateNumbers("nickels", "Nickels: " + nickels);
+  updateNumbers("pennies", "Pennies: " + pennies);
 }
