805f01be91a8fcc51716b53e2c01cf632f8b8625
TheTabbingMan <gamerboyjjh@outlook.com>
2024-12-20 20:24:18 -0600

    Optimize calculate e

--- My-Numbers/script.js
+++ My-Numbers/script.js
@@ -12,7 +12,7 @@ function oldFactorialDecimal(n, precision) {
   if (n === 0 || n === 1) {
     return new Decimal(1);
   }
-  const factorial = new Decimal(n * factorialDecimal(n - 1));
+  const factorial = new Decimal(n * oldFactorialDecimal(n - 1));
   return factorial;
 }
 
@@ -26,19 +26,20 @@ function factorialDecimal(n) {
 
 /**
  * Calculate the number e with the specified number of digits
- * @param {number} digits - Number of digits to calculate
+ * @param {number} digits - Number of decimal digits to calculate
  * @returns {string} - Digits of e specified
  */
 function calculate_e(digits) {
   Decimal.set({ precision: digits + 3 });
   let e = new Decimal(2);
+  let factorial = new Decimal(1);
   for (let i = 1; i < digits + 3; i++) {
     const numerator = new Decimal(2 * i + 2);
-    const denominator = new Decimal(factorialDecimal(2 * i + 1));
-    e = e.plus(numerator.div(denominator));
+    factorial = factorial.times(2 * i).times(2 * i + 1);
+    e = e.plus(numerator.div(factorial));
   }
   const result = e.toString().slice(0, digits + 2);
   return result;
 }
 
-console.log(calculate_e(10000));
+console.log(calculate_e(9999));
