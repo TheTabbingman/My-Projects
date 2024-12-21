139fedd658273331edb7fb0c50d69a72f535b910
TheTabbingMan <gamerboyjjh@outlook.com>
2024-12-20 19:19:30 -0600

    Update ESLint configuration for module support and add decimal.js dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import jsdoc from "eslint-plugin-jsdoc";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   jsdoc.configs["flat/recommended"],
