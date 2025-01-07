f106d41ff8a35e2f052cdfb7bd1de0108f2538eb
TheTabbingMan <gamerboyjjh@outlook.com>
2025-01-06 20:43:23 -0600

    Convert to class

--- Color-Cycle/script.js
+++ Color-Cycle/script.js
@@ -9,83 +9,96 @@ const blueOffset = document.getElementById("blueOffset");
 const intervalInput = document.getElementById("intervalInput");
 const intervalApplyButton = document.getElementById("applyInterval");
 
-let running = false;
-let offsetInterval = null;
-let intervalAmount = 250;
+class ColorCycler {
+  constructor() {
+    this.running = false;
+    this.offsetInterval = null;
+    this.intervalAmount = 250;
+  }
 
-function setColor(inputColorDictionary) {
-  colorBox.style.backgroundColor = `rgb(${inputColorDictionary["red"]}, ${inputColorDictionary["green"]}, ${inputColorDictionary["blue"]})`;
-}
+  setColor(inputColorDictionary) {
+    colorBox.style.backgroundColor = `rgb(${inputColorDictionary["red"]}, ${inputColorDictionary["green"]}, ${inputColorDictionary["blue"]})`;
+  }
 
-function offsetColor() {
-  const redOffsetValue = redOffset.value;
-  const greenOffsetValue = greenOffset.value;
-  const blueOffsetValue = blueOffset.value;
-  const colorBoxStyle = window.getComputedStyle(colorBox);
+  offsetColor() {
+    const redOffsetValue = redOffset.value;
+    const greenOffsetValue = greenOffset.value;
+    const blueOffsetValue = blueOffset.value;
+    const colorBoxStyle = window.getComputedStyle(colorBox);
 
-  const colorBoxStyleDictionary = convertToRgb(colorBoxStyle.backgroundColor);
-  colorBoxStyleDictionary["red"] += Number(redOffsetValue);
-  colorBoxStyleDictionary["green"] += Number(greenOffsetValue);
-  colorBoxStyleDictionary["blue"] += Number(blueOffsetValue);
-  setColor(colorBoxStyleDictionary);
-}
+    const colorBoxStyleDictionary = this.convertToRgb(
+      colorBoxStyle.backgroundColor,
+    );
+    colorBoxStyleDictionary["red"] += Number(redOffsetValue);
+    colorBoxStyleDictionary["green"] += Number(greenOffsetValue);
+    colorBoxStyleDictionary["blue"] += Number(blueOffsetValue);
+    this.setColor(colorBoxStyleDictionary);
+  }
 
-function convertToRgb(rgbString) {
-  const rgbArray = rgbString.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
-  if (rgbArray) {
-    return {
-      red: Number(rgbArray[1]),
-      green: Number(rgbArray[2]),
-      blue: Number(rgbArray[3]),
-    };
+  convertToRgb(rgbString) {
+    const rgbArray = rgbString.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
+    if (rgbArray) {
+      return {
+        red: Number(rgbArray[1]),
+        green: Number(rgbArray[2]),
+        blue: Number(rgbArray[3]),
+      };
+    }
+  }
+
+  formatGlobalColor() {
+    const startingColorHexTemp = startingColorInput.value.match(
+      /^#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})$/,
+    );
+    const startingColorRgbTemp = startingColorInput.value.match(
+      /(\d+),\s*(\d+),\s*(\d+)/,
+    );
+    let rgbStartingColor;
+    if (startingColorHexTemp) {
+      rgbStartingColor = {
+        red: parseInt(startingColorHexTemp[1], 16),
+        green: parseInt(startingColorHexTemp[2], 16),
+        blue: parseInt(startingColorHexTemp[3], 16),
+      };
+    }
+    if (startingColorRgbTemp) {
+      rgbStartingColor = {
+        red: Number(startingColorRgbTemp[1]),
+        green: Number(startingColorRgbTemp[2]),
+        blue: Number(startingColorRgbTemp[3]),
+      };
+    }
+    return rgbStartingColor;
+  }
+
+  changeInterval() {
+    this.intervalAmount = intervalInput.value;
+    colorBox.style.transitionDuration = `${this.intervalAmount / 1000}s`;
+    if (this.running) {
+      clearInterval(this.offsetInterval);
+      this.offsetInterval = setInterval(this.offsetColor, this.intervalAmount);
+    }
   }
 }
 
-function formatGlobalColor() {
-  const startingColorHexTemp = startingColorInput.value.match(
-    /^#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})$/,
-  );
-  const startingColorRgbTemp = startingColorInput.value.match(
-    /(\d+),\s*(\d+),\s*(\d+)/,
-  );
-  let rgbStartingColor;
-  if (startingColorHexTemp) {
-    rgbStartingColor = {
-      red: parseInt(startingColorHexTemp[1], 16),
-      green: parseInt(startingColorHexTemp[2], 16),
-      blue: parseInt(startingColorHexTemp[3], 16),
-    };
-  }
-  if (startingColorRgbTemp) {
-    rgbStartingColor = {
-      red: Number(startingColorRgbTemp[1]),
-      green: Number(startingColorRgbTemp[2]),
-      blue: Number(startingColorRgbTemp[3]),
-    };
-  }
-  return rgbStartingColor;
-}
-
-function changeInterval() {
-  intervalAmount = intervalInput.value;
-  colorBox.style.transitionDuration = `${intervalAmount / 1000}s`;
-  if (running) {
-    clearInterval(offsetInterval);
-    offsetInterval = setInterval(offsetColor, intervalAmount);
-  }
-}
+const colorCycler = new ColorCycler();
 
 startButton.addEventListener("click", () => {
-  if (!running) {
-    setColor(formatGlobalColor());
-    offsetInterval = setInterval(offsetColor, intervalAmount);
+  if (!colorCycler.running) {
+    colorCycler.setColor(colorCycler.formatGlobalColor());
+    colorCycler.offsetInterval = setInterval(
+      colorCycler.offsetColor.bind(colorCycler),
+      colorCycler.intervalAmount,
+    );
     startButton.innerText = "Stop";
   } else {
-    clearInterval(offsetInterval);
-    offsetInterval = null;
+    clearInterval(colorCycler.offsetInterval);
+    colorCycler.offsetInterval = null;
     startButton.innerText = "Start";
   }
-  running = !running;
+  colorCycler.running = !colorCycler.running;
 });
 
-intervalApplyButton.addEventListener("click", changeInterval);
+intervalApplyButton.addEventListener("click", () => {
+  colorCycler.changeInterval();
+});
